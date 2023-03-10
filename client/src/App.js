fcc2e873143e80f013d1c092d990123e79e73ff0
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-03-10 10:38:23 +0530

    Routing Setup✅

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,31 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import UseModel from "./pages/UseModel";
+import Register from "./pages/Register";
+import MyComplaints from "./pages/MyComplaints";
+import AdminComplaints from "./pages/AdminComplaints";
+import Profile from "./pages/Profile";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/usemodel" element={<UseModel />} />
+          <Route path="/registercomplaint" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/mycomplaints" element={<MyComplaints />} />
+          <Route path="/allcomplaints" element={<AdminComplaints />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
+
+export default App;
