e96ebd3eada11c1b5461650f26f782952e17df3d
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-03-11 10:14:41 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -58,9 +58,13 @@ const Navbar = () => {
             <div className="hidden lg:flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5 -ml-8">
               <Link
                 to="/login"
-                className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+                className={
+                  user
+                    ? "bg-gray-200 px-2 py-3 rounded-lg"
+                    : "inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+                }
               >
-                {user ? "Logout" : "Login"}
+                {user ? user.displayName : "Login"}
               </Link>
             </div>
             <div className="lg:hidden flex flex-1 justify-end items-center py-5">
